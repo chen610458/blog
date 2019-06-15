48b9114d20f28d00a77322b85b8d0631900867a8
chen610458@gmail.com <chen610458@gmail.com>
2019-06-15 13:28:21 +0800

    push 1

---
+++ blog/main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# from django.http import HttpResponse
+
+
+def main(request):
+    '''
+    Show 'Hello world!' in the main page
+    '''
+    
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
+
+from django.http import HttpResponse
+
+
