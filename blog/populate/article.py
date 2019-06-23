b4875ec965895165865aa032c6e18268ee9afb5d
chen610458@gmail.com <chen610458@gmail.com>
2019-06-23 13:32:31 +0800

    push6

--- blog/populate/article.py
+++ blog/populate/article.py
@@ -1,4 +1,5 @@
 from populate import base
+from account.models import User
 from article.models import Article, Comment
 
 
@@ -11,6 +12,7 @@ def populate():
     Article.objects.all().delete()
     Comment.objects.all().delete()
 
+    admin = User.objects.get(is_superuser=True)
     for title in titles:
         article = Article()
         article.title = title
@@ -18,7 +20,7 @@ def populate():
             article.content += title + '\n'
         article.save()
         for comment in comments:
-            Comment.objects.create(article=article, content=comment)
+            Comment.objects.create(article=article, user=admin, content=comment)
     print('done')
 
 
