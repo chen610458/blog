16f3153ee4ef896cf821f8dc09fe5f787c7e91bb
chen610458@gmail.com <chen610458@gmail.com>
2019-06-23 10:16:23 +0800

    push5

--- blog/account/views.py
+++ blog/account/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
 from account.forms import UserForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 
 
 def register(request):
@@ -21,3 +24,35 @@ def register(request):
     messages.success(request, '歡迎註冊')
     return redirect('main:main')
 
+def login(request):
+    '''
+    Login an existing user
+    '''
+    template = 'account/login.html'
+    if request.method == 'GET':
+        return render(request, template)
+
+    # POST
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not username or not password:    # Server-side validation
+        messages.error(request, '請填資料')
+        return render(request, template)
+
+    user = authenticate(username=username, password=password)
+    if not user:    # authentication fails
+        messages.error(request, '登入失敗')
+        return render(request, template)
+
+    # login success
+    auth_login(request, user)
+    messages.success(request, '登入成功')
+    return redirect('main:main')
+
+def logout(request):
+    '''
+    Logout the user
+    '''
+    auth_logout(request)
+    messages.success(request, '歡迎再度光臨')
+    return redirect('main:main')
